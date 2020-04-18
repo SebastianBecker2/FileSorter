da2885136369910a450441114dafd5b704445cb3
Sebastian Becker <mail@sbecker.de.com>
2020-04-18 22:03:16 +0200

    Add filter and recent items to reassign destiation
    The AssignDestination dialogs performance has been improved.
    A filter textbox has been added to filter by key in destination items.
    Recently selected destination items have been added for easy selection.

--- FileSorter/AssignDestination.cs
+++ FileSorter/AssignDestination.cs
@@ -2,17 +2,20 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MoreLinq;
 
 namespace FileSorter
 {
     public partial class AssignDestination : Form
     {
-        public IOrderedEnumerable<DestinationItem> DestinationItems { get; set; }
+        public IEnumerable<DestinationItem> DestinationItems { get; set; }
+        public IEnumerable<DestinationItem> RecentDestinationItems { get; set; }
         public DestinationItem SelectedDestination { get; set; }
 
         public AssignDestination()
@@ -23,15 +26,46 @@ namespace FileSorter
         protected override void OnLoad(EventArgs e)
         {
             DisplayDestinationItems(DestinationItems);
+            DisplayRecentDestinationItems(RecentDestinationItems);
+
+            DgvDestinations.ClearSelection();
+            DgvRecentDestinations.ClearSelection();
 
             base.OnLoad(e);
         }
 
-        private void DisplayDestinationItems(IOrderedEnumerable<DestinationItem> destination_items)
+        private void DisplayRecentDestinationItems(IEnumerable<DestinationItem> recent_destination_items)
+        {
+            DgvRecentDestinations.Rows.Clear();
+
+            foreach (var item in recent_destination_items.Reverse())
+            {
+                var row = new DataGridViewRow()
+                {
+                    Tag = item,
+                };
+
+                var cell = new DataGridViewTextBoxCell()
+                {
+                    Value = item.Key,
+                };
+                row.Cells.Add(cell);
+
+                cell = new DataGridViewTextBoxCell()
+                {
+                    Value = item.Path,
+                };
+                row.Cells.Add(cell);
+
+                DgvRecentDestinations.Rows.Add(row);
+            }
+        }
+
+        private void DisplayDestinationItems(IEnumerable<DestinationItem> destination_items)
         {
             DgvDestinations.Rows.Clear();
 
-            foreach (var item in destination_items)
+            foreach (var item in destination_items.DistinctBy(i => i.Path))
             {
                 var row = new DataGridViewRow()
                 {
@@ -58,14 +92,52 @@ namespace FileSorter
 
         private void BtnOkay_Click(object sender, EventArgs e)
         {
-            if (DgvDestinations.SelectedRows.Count != 1)
+            if (DgvDestinations.SelectedRows.Count == 1)
             {
-                return;
+                SelectedDestination = DgvDestinations.SelectedRows[0].Tag as DestinationItem;
             }
 
-            SelectedDestination = DgvDestinations.SelectedRows[0].Tag as DestinationItem;
-
+            if (DgvRecentDestinations.SelectedRows.Count == 1)
+            {
+                SelectedDestination = DgvRecentDestinations.SelectedRows[0].Tag as DestinationItem;
+            }
+            
             DialogResult = DialogResult.OK;
+        }
+
+        private void TxtFilter_TextChanged(object sender, EventArgs e)
+        {
+            var filtered_items = DestinationItems.Where(i => i.Key.Contains(TxtFilter.Text));
+
+            DisplayDestinationItems(filtered_items);
+        }
+
+        private void DgvDestinations_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DgvDestinations.Rows[e.RowIndex].Selected = true;
+            BtnOkay.PerformClick();
+        }
+
+        private void DgvRecentDestinations_CellMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            DgvRecentDestinations.Rows[e.RowIndex].Selected = true;
+            BtnOkay.PerformClick();
+        }
+
+        private void DgvDestinations_SelectionChanged(object sender, EventArgs e)
+        {
+            if (DgvDestinations.SelectedRows.Count > 0)
+            {
+                DgvRecentDestinations.ClearSelection();
+            }
+        }
+
+        private void DgvRecentDestinations_SelectionChanged(object sender, EventArgs e)
+        {
+            if (DgvRecentDestinations.SelectedRows.Count > 0)
+            {
+                DgvDestinations.ClearSelection();
+            }
         }
     }
 }

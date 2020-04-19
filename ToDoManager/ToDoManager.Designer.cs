cd23088d02f5d65372a089b201e7c9ba13f43f94
Sebastian Becker <mail@sbecker.de.com>
2020-04-19 14:41:49 +0200

    Add ToDo manager
    ToDo manager as a separate project for reusability.
    It allows us to handle the ToDos of the project right in there.
    Though the ToDos are stored in the properties settings as of right now

---
+++ ToDoManager/ToDoManager.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace ToDoManager
+{
+    partial class ToDoManager
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnCancel = new System.Windows.Forms.Button();
+            this.BtnOkay = new System.Windows.Forms.Button();
+            this.DgvToDoList = new System.Windows.Forms.DataGridView();
+            this.DgcDone = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.DgcText = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DgcDelete = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.ChbHideCompletedItems = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvToDoList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnCancel
+            // 
+            this.BtnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.BtnCancel.Location = new System.Drawing.Point(713, 415);
+            this.BtnCancel.Name = "BtnCancel";
+            this.BtnCancel.Size = new System.Drawing.Size(75, 23);
+            this.BtnCancel.TabIndex = 17;
+            this.BtnCancel.Text = "Cancel";
+            this.BtnCancel.UseVisualStyleBackColor = true;
+            // 
+            // BtnOkay
+            // 
+            this.BtnOkay.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnOkay.Location = new System.Drawing.Point(632, 415);
+            this.BtnOkay.Name = "BtnOkay";
+            this.BtnOkay.Size = new System.Drawing.Size(75, 23);
+            this.BtnOkay.TabIndex = 16;
+            this.BtnOkay.Text = "OK";
+            this.BtnOkay.UseVisualStyleBackColor = true;
+            this.BtnOkay.Click += new System.EventHandler(this.BtnOkay_Click);
+            // 
+            // DgvToDoList
+            // 
+            this.DgvToDoList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.DgvToDoList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgvToDoList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.DgcDone,
+            this.DgcText,
+            this.DgcDelete});
+            this.DgvToDoList.Location = new System.Drawing.Point(12, 12);
+            this.DgvToDoList.MultiSelect = false;
+            this.DgvToDoList.Name = "DgvToDoList";
+            this.DgvToDoList.RowHeadersVisible = false;
+            this.DgvToDoList.Size = new System.Drawing.Size(776, 397);
+            this.DgvToDoList.TabIndex = 18;
+            this.DgvToDoList.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DgvToDoList_CellContentClick);
+            this.DgvToDoList.DefaultValuesNeeded += new System.Windows.Forms.DataGridViewRowEventHandler(this.DgvToDoList_DefaultValuesNeeded);
+            // 
+            // DgcDone
+            // 
+            this.DgcDone.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.DgcDone.HeaderText = "Done";
+            this.DgcDone.Name = "DgcDone";
+            this.DgcDone.Width = 39;
+            // 
+            // DgcText
+            // 
+            this.DgcText.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.DgcText.HeaderText = "Text";
+            this.DgcText.Name = "DgcText";
+            // 
+            // DgcDelete
+            // 
+            this.DgcDelete.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.DgcDelete.HeaderText = "Delete";
+            this.DgcDelete.Name = "DgcDelete";
+            this.DgcDelete.Width = 44;
+            // 
+            // ChbHideCompletedItems
+            // 
+            this.ChbHideCompletedItems.AutoSize = true;
+            this.ChbHideCompletedItems.Checked = true;
+            this.ChbHideCompletedItems.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.ChbHideCompletedItems.Location = new System.Drawing.Point(12, 419);
+            this.ChbHideCompletedItems.Name = "ChbHideCompletedItems";
+            this.ChbHideCompletedItems.Size = new System.Drawing.Size(127, 17);
+            this.ChbHideCompletedItems.TabIndex = 19;
+            this.ChbHideCompletedItems.Text = "Hide completed items";
+            this.ChbHideCompletedItems.UseVisualStyleBackColor = true;
+            this.ChbHideCompletedItems.CheckedChanged += new System.EventHandler(this.ChbHideCompletedItems_CheckedChanged);
+            // 
+            // ToDoManager
+            // 
+            this.AcceptButton = this.BtnOkay;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.BtnCancel;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ChbHideCompletedItems);
+            this.Controls.Add(this.DgvToDoList);
+            this.Controls.Add(this.BtnCancel);
+            this.Controls.Add(this.BtnOkay);
+            this.Name = "ToDoManager";
+            this.Text = "ToDo Manager";
+            ((System.ComponentModel.ISupportInitialize)(this.DgvToDoList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnCancel;
+        private System.Windows.Forms.Button BtnOkay;
+        private System.Windows.Forms.DataGridView DgvToDoList;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn DgcDone;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DgcText;
+        private System.Windows.Forms.DataGridViewButtonColumn DgcDelete;
+        private System.Windows.Forms.CheckBox ChbHideCompletedItems;
+    }
+}
---
+++ ToDoManager/ToDoManager.cs
@@ -0,0 +1,139 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ToDoManager.Properties;
+
+namespace ToDoManager
+{
+    public partial class ToDoManager : Form
+    {
+        List<ToDoItem> ToDoItems
+        {
+            get { return JsonConvert.DeserializeObject<List<ToDoItem>>(Settings.Default.ToDoListItem); }
+            set
+            {
+                Settings.Default.ToDoListItem = JsonConvert.SerializeObject(value);
+                Settings.Default.Save();
+            }
+        }
+
+        public ToDoManager()
+        {
+            InitializeComponent();
+
+            if (ToDoItems == null)
+            {
+                ToDoItems = new List<ToDoItem>();
+            }
+
+            DgvToDoList.Columns["DgcText"].DefaultCellStyle.WrapMode = DataGridViewTriState.True;
+            DgvToDoList.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            DisplayToDoList(ToDoItems);
+
+            ApplyFilterToDgv();
+
+            base.OnLoad(e);
+        }
+
+        private void DisplayToDoList(IEnumerable<ToDoItem> items)
+        {
+            DgvToDoList.Rows.Clear();
+
+            foreach (var item in items.OrderBy(i => i.Done))
+            {
+                var row = new DataGridViewRow();
+
+                var chb_cell = new DataGridViewCheckBoxCell()
+                {
+                    Value = item.Done,
+                };
+                row.Cells.Add(chb_cell);
+
+                var text_cell = new DataGridViewTextBoxCell()
+                {
+                    Value = item.Text,
+                };
+                row.Cells.Add(text_cell);
+
+                var delete_cell = new DataGridViewButtonCell()
+                {
+                    Value = "X",
+                    
+                };
+                row.Cells.Add(delete_cell);
+
+                DgvToDoList.Rows.Add(row);
+            }
+        }
+
+        private List<ToDoItem> GetToDoItemsFromDgv()
+        {
+            return DgvToDoList.Rows
+                .Cast<DataGridViewRow>()
+                .Where(r => !r.IsNewRow)
+                .Select(r => new ToDoItem
+                {
+                    Done = (bool)(r.Cells["DgcDone"] as DataGridViewCheckBoxCell).Value,
+                    Text = r.Cells["DgcText"].Value as string
+                })
+                .ToList();
+        }
+
+        private void BtnOkay_Click(object sender, EventArgs e)
+        {
+            ToDoItems = GetToDoItemsFromDgv();
+
+            DialogResult = DialogResult.OK;
+        }
+
+        private void DgvToDoList_DefaultValuesNeeded(object sender, DataGridViewRowEventArgs e)
+        {
+            e.Row.Cells["DgcDone"].Value = false;
+            e.Row.Cells["DgcText"].Value = "";
+            e.Row.Cells["DgcDelete"].Value = "X";
+        }
+
+        private void DgvToDoList_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (DgvToDoList.Rows[e.RowIndex].IsNewRow)
+            {
+                return;
+            }
+
+            if (DgvToDoList.Columns["DgcDelete"].Index != e.ColumnIndex)
+            {
+                return;
+            }
+
+            DgvToDoList.Rows.RemoveAt(e.RowIndex);
+        }
+
+        private void ChbHideCompletedItems_CheckedChanged(object sender, EventArgs e)
+        {
+            ApplyFilterToDgv();
+        }
+
+        private void ApplyFilterToDgv()
+        {
+            foreach (var row in DgvToDoList.Rows
+                .Cast<DataGridViewRow>()
+                .Where(r => !r.IsNewRow)
+                .Where(r => (bool)(r.Cells["DgcDone"] as DataGridViewCheckBoxCell).Value))
+            {
+                row.Visible = !ChbHideCompletedItems.Checked;
+            }
+        }
+    }
+}
